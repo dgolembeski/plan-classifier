a5514ae7dc56806d718749197c7c03cf45f20d7f
dgolembeski <daniel.golembeski@gmail.com>
2025-05-27 14:15:17 -0400

    Delete api directory

--- api/classify.js
+++
@@ -1,7 +0,0 @@
-import classifier from "../lib/classifier.js";   // move your classifyCard code into /lib
-
-export default function handler(req, res) {
-  if (req.method !== "POST") return res.status(405).end();
-  const result = classifier(req.body || {});
-  return res.json(result);
-}
